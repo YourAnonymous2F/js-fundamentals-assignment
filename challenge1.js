bac3df5f8bfa688099f5eb2031ebbb6e73387873
Fohdil <fohdilulahiolayanju@gmail.com>
2025-12-17 02:09:56 -0600

    added all the assignment solution file I created initially on my system to the cloned repository

---
+++ challenge1.js
@@ -0,0 +1,15 @@
+// Personal Information
+
+// Creating variables to store my personal information and display them in formatted string. Requirements : Use const for values that won't change
+
+const myFullName = "Fohdilulahi Olayanju";
+
+let myAge = 24;
+
+const favouriteProgrammingLanguage = "Javascript";
+
+let isStudent = false;
+
+let currentYear = 2025
+
+console.log (`My full name is ${myFullName}. I'm ${myAge} years old and my favourite programming language I'm learning is ${favouriteProgrammingLanguage}. The boolean value for I'm a student is ${isStudent} and the current year we are in is ${2025}.`)
---
+++ challenge2.js
@@ -0,0 +1,24 @@
+// Simple Calculator
+// Task: Simple calculator that performs basic arithmetic operations on two numbers
+
+// declaring variables for the two numbers
+let num1 = 10;
+let num2 = 4;
+
+// ADDITION
+console.log (num1 + num2);
+
+// SUBTRACTION
+console.log (num1 - num2);
+
+// MULTIPLICATION
+console.log (num1 * num2);
+
+// DIVISION
+console.log (num1 / num2);
+
+// MODULUS (REMINDER)
+console.log (num1 % num2);
+
+// EXPONENTIATION
+console.log (num1 ** num2);
