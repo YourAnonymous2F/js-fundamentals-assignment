e60983daa3239963e697bd4656fe931f26d7fd04
Fohdil <fohdilulahiolayanju@gmail.com>
2025-12-20 15:06:03 -0600

    made little changes to the challenge 3 js file

--- challenge3.js
+++ challenge3.js
@@ -1,10 +1,20 @@
 // for loop to check whether a number is even or odd from 1-20
 
-for (let i = 1; i <= 20; i++) {
-    if (i % 2 == 0) {
-        console.log (`${i} is an even number`);
+for (let number = 1; number <= 20; number++) {
+    if (number % 2 == 0) {
+        console.log (`${number} is an even number`);
     }
     else {
-        console.log (`${i} is an odd number`)
+        console.log (`${number} is an odd number`)
     }
 }
+
+function countNumbers (numberRAnge) {
+    let even = 0, odd = 0;
+    for (let number = 1; number <= numberRAnge; number++) {
+        if (number % 2 === 0) even ++;
+            else odd++
+    }        
+    return `${even} even numbers, ${odd} odd numbers`
+}
+console.log(countNumbers(20))
